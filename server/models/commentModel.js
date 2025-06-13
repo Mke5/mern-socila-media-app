f9e7ed6bc95c7f35bc2b2e90cf39aa3471bd8eb0
mke5 <emma08062602618@gmail.com>
2025-06-13 17:02:23 +0100

    comment model

---
+++ server/models/commentModel.js
@@ -0,0 +1,41 @@
+const {Schema, model} = require('mongoose')
+
+const commentSchema = new Schema({
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    creatorName: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 50,
+    },
+    creatorAvatar: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 200
+    },
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true,
+        index: true
+    },
+    comment: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 500
+    }
+})
+
+
+
+
+module.exports = model('Comment', commentSchema)
