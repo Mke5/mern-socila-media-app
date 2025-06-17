fdd7fb5b7466dfe929a3c2ea6a95a94a99287e60
mke5 <emma08062602618@gmail.com>
2025-06-17 15:30:12 +0100

    frontend'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './RootLayout'
+import ErrorPage from './pages/ErrorPage'
+import Home from './pages/Home'
+import MessagesList from './components/MessagesList'
+import Messages from './pages/Messages'
+import Bookmarks from './pages/Bookmarks'
+import Profile from './pages/Profile'
+import SinglePost from './pages/SinglePost'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Logout from './pages/Logout'
 
 
 
-
+const router = createBrowserRouter([
+  {path: '/', element: <RootLayout />, errorElement: <ErrorPage />, children: [
+    {index: true, element: <Home />},
+    {path: 'messages', element: <MessagesList />},
+    {path: 'messages/:recieverId', element: <Messages />},
+    {path: 'bookmarks/', element: <Bookmarks />},
+    {path: 'users/:id', element: <Profile />},
+    {path: 'posts/:id', element: <SinglePost />},
+  ]},
+  {path: '/login', element: <Login />},
+  {path: '/register', element: <Register />},
+  {path: '/logout', element: <Logout />}
+])
 
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
--- client/src/RootLayout.jsx
+++ client/src/RootLayout.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Widget from './components/Widget'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div>
+       <Navbar />
+       <main className="main">
+            <div className="container main__container">
+                <Sidebar />
+                <Outlet />
+                <Widget />
+            </div>
+        </main> 
+    </div>
   )
 }
 
