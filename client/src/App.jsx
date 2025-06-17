5c49891e58831fecdb1068d7e8f2d1cd54bc2c68
mke5 <emma08062602618@gmail.com>
2025-06-17 14:54:02 +0100

    frontend'

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import RootLayout from './RootLayout'
+
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ client/src/pages/SinglePost.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SinglePost = () => {
+  return (
+    <div>
+      SinglePost
+      SinglePost
+    </div>
+  )
+}
+
+export default SinglePost
